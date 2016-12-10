eb42dfa190588cd7805b38d1ef487a6a210aaaab
Marcelo <marcelo.bohn@consyste.com.br>
2016-12-10 18:18:09 -0200

    define state vars for form

--- src/GameForm.js
+++ src/GameForm.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GameForm extends React.Component {
+  state = {
+    title: '',
+    cover: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
     return (
         <form className="ui form">
@@ -9,6 +18,9 @@ class GameForm extends React.Component {
           <div className="field">
             <label htmlFor="title">Title</label>
             <input
+                name="title"
+                value={this.state.title}
+                onChange={this.handleChange}
                 id="title"
             />
           </div>
@@ -16,12 +28,15 @@ class GameForm extends React.Component {
           <div className="field">
             <label htmlFor="cover">Cover</label>
             <input
+                name="cover"
+                value={this.state.cover}
+                onChange={this.handleChange}
                 id="cover"
             />
           </div>
 
           <div className="field">
-            <img src="" alt="cover" className="ui small bordered image"/>
+            { this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image"/> }
           </div>
 
           <div className="field">
