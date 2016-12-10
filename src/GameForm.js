aa5a936acb7e227729cf7f02bf28325152c63579
Marcelo <marcelo.bohn@consyste.com.br>
2016-12-10 17:33:41 -0200

    add semantic and component for new game

---
+++ src/GameForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GameForm extends React.Component {
+  render() {
+    return (
+        <form>
+          <h1>Add new game</h1>
+
+
+        </form>
+    );
+  }
+}
+
+
+export default GameForm
